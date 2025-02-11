54f6272399c09072a2652a8559b731030473645f
LENOVO <hfz.snyl@gmail.com>
2025-02-11 19:14:04 +0300

    2. Week

---
+++ src/main/java/com/hafizesenyil/_2_week/_10_1_Loop_For.java
@@ -0,0 +1,15 @@
+package com.hafizesenyil._2_week;
+
+public class _10_1_Loop_For {
+    public static void main(String[] args) {
+        // i++
+        // i=i+1
+        // i+=1
+        for (int i = 1; i <=10 ; i++) {
+            System.out.print(i+" - ");
+        }
+
+        // for döngüsü sonsuzluk
+        // for(;;){}
+    }
+}
